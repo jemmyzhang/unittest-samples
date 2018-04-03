ff3a662aa16f01876f97efe3f0e7dc8e7a604011
Jemmy Zhang <hzdan360@163.com>
2018-04-03 23:02:40 +0800

    bug fixed

--- src/test/java/pers/jz/unittest/test/ExampleRestClientTest.java
+++ src/test/java/pers/jz/unittest/test/ExampleRestClientTest.java
@@ -3,9 +3,13 @@ package pers.jz.unittest.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.web.client.MockServerRestTemplateCustomizer;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestTemplate;
+import pers.jz.unittest.config.WebConfig;
 import pers.jz.unittest.service.RestService;
 
 import javax.annotation.Resource;
@@ -19,19 +23,27 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
  */
 @RunWith(SpringRunner.class)
 @RestClientTest(RestService.class)
+@Import(WebConfig.class)
 public class ExampleRestClientTest {
+
+    @Resource
+    private RestTemplate restTemplate;
 
     @Resource(name = "restService")
     private RestService restService;
+
+    @Resource
+    MockServerRestTemplateCustomizer mockServerRestTemplateCustomizer;
 
     @Resource
     private MockRestServiceServer mockRestServiceServer;
 
     @Test
     public void testRestResult() throws Exception {
-        mockRestServiceServer.expect(requestTo("/")).andRespond(withSuccess("root", MediaType.TEXT_PLAIN));
+        mockServerRestTemplateCustomizer.customize(restTemplate);
+        mockRestServiceServer.expect(requestTo("http://localhost:8080/")).andRespond(withSuccess("tomcat8", MediaType.TEXT_PLAIN));
         String call = restService.invokeRoot();
-        assertThat(call).isEqualTo("root");
+        assertThat(call).isEqualTo("tomcat8");
     }
 
 }
