939f4c521ae0e40296ce898ed3cc2a992ff0330e
Jemmy Zhang <hzdan360@163.com>
2018-04-03 21:06:56 +0800

    write bugs

--- src/test/java/pers/jz/unittest/test/ExampleRestClientTest.java
+++ src/test/java/pers/jz/unittest/test/ExampleRestClientTest.java
@@ -1,13 +1,37 @@
 package pers.jz.unittest.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.client.MockRestServiceServer;
+import pers.jz.unittest.service.RestService;
+
+import javax.annotation.Resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 /**
  * @author jemmyzhang on 2018/4/3.
  */
 @RunWith(SpringRunner.class)
-@RestClientTest
+@RestClientTest(RestService.class)
 public class ExampleRestClientTest {
+
+    @Resource(name = "restService")
+    private RestService restService;
+
+    @Resource
+    private MockRestServiceServer mockRestServiceServer;
+
+    @Test
+    public void testRestResult() throws Exception {
+        mockRestServiceServer.expect(requestTo("/")).andRespond(withSuccess("root", MediaType.TEXT_PLAIN));
+        String call = restService.invokeRoot();
+        assertThat(call).isEqualTo("root");
+    }
+
 }
--- src/test/java/pers/jz/unittest/test/MyControllerTest.java
+++ src/test/java/pers/jz/unittest/test/MyControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import pers.jz.unittest.controller.MathController;
 import pers.jz.unittest.controller.UserController;
 import pers.jz.unittest.entity.User;
-import pers.jz.unittest.service.MathService;
 import pers.jz.unittest.service.UserService;
 
 import javax.annotation.Resource;
